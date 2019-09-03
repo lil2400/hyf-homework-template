040054ccea999f818096440adfb528c8f2e3a0f8
lil2400 <31505212+lil2400@users.noreply.github.com>
2019-09-04 01:35:07 +0200

    Removed comment

--- react/week1/to-do-list/src/App.js
+++ react/week1/to-do-list/src/App.js
@@ -4,7 +4,6 @@ import toDoList from "./toDoList";
 class App extends React.Component {
   render() {
     return toDoList.map((item, index) => (
-      // Only do this if items have no stable IDs
       <li key={index}>{item.content}</li>
     ));
   }
